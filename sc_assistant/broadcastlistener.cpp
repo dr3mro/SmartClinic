9d2e6cbb67af6e0aaa298b88192efbff167d1c1d
Amr <dr3mro@gmail.com>
2021-07-22 02:03:33 +0200

    **

--- sc_assistant/broadcastlistener.cpp
+++ sc_assistant/broadcastlistener.cpp
@@ -4,9 +4,7 @@ broadcastListener::broadcastListener(QString &ip_address, QObject *parent) : QOb
     udpSocket(new QUdpSocket(this)),
     m_ip_address(ip_address)
 {
-    udpSocket->bind(QHostAddress::AnyIPv4, 45454, QUdpSocket::ShareAddress);
-    udpSocket->joinMulticastGroup(QHostAddress::AnyIPv4);
-
+    udpSocket->bind(QHostAddress::AnyIPv4,45454,QUdpSocket::ShareAddress | QUdpSocket::ReuseAddressHint);
 
     connect(udpSocket, &QUdpSocket::readyRead,
             this, &broadcastListener::processPendingDatagrams);
@@ -20,11 +18,13 @@ broadcastListener::~broadcastListener()
 void broadcastListener::processPendingDatagrams()
 {
     QByteArray datagram;
+    QHostAddress host;
     while (udpSocket->hasPendingDatagrams()) {
         datagram.resize(int(udpSocket->pendingDatagramSize()));
-        udpSocket->readDatagram(datagram.data(), datagram.size());
-        m_ip_address = QString(datagram.constData());
-        qDebug() << m_ip_address;
+        udpSocket->readDatagram(datagram.data(), datagram.size(),&host);
+        if(datagram == "hello")
+            m_ip_address = host.toString();
+        //qDebug() << m_ip_address;
     }
 }
 
