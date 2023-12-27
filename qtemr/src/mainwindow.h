575625a5145168d43a7734093e066f8c6a09d2b4
Dr Amr Osman <dr3mro@gmail.com>
2023-12-27 10:40:59 +0200

    * clean

--- qtemr/src/mainwindow.h
+++ qtemr/src/mainwindow.h
@@ -1,8 +1,4 @@
-﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
-
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-
-#ifndef MAINWINDOW_H
+﻿#ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
